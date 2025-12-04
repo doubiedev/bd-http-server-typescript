a1005343028442b593b93f20b381a3c39ba0695f
doubiedev <thedoubiedev@gmail.com>
2025-12-04 15:04:33 +1100

    match ch1 solution

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-import express, { Request, Response } from "express";
+import express from "express";
+
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
-app.get("/healthz", handlerReadiness);
-
-function handlerReadiness(req: Request, res: Response) {
-    res.set({ "Content-Type": "text/plain", charset: "utf-8" });
-    res.status(200).send("OK");
-}
 
 app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
